ddf2621f09f117fb50abef7e60746981725fdc60
Siddharth <soni28siddh@email.com>
2024-07-28 17:27:14 +0530

    SS: fixed bg img path

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,6 @@ export default {
         // Configure your color palette here
         primary: "#D29A5A",
       },
-      backgroundImage: {
-        hero: "linear-gradient(rgba(0, 0, 0, 9%),rgba(0, 0, 0, 0.5)),url('/assets/hero-bg.jpeg')",
-        promotion: "url('./assets/bg-1.png')",
-        newletter:
-          " linear-gradient(rgba(0, 0, 0, 0.527),rgba(0, 0, 0, 0.5)) ,url('/assets/bg-2.jpeg')",
-      },
     },
   },
   plugins: [],
